b9da0970ab72db7f1153dc3153f9c473643fffda
Pat <patrick.cheurfa@gmail.com>
2019-01-08 15:52:52 +0100

    Updated livre.php.

---
+++ livredor4/livre.php
@@ -0,0 +1,11 @@
+<form method="POST" action"livre.php">
+  <hr>
+  <strong>Poster un commentaire</strong>
+  <p>Votre nom :</p>
+  <input type="text" name="nom">
+  <p>Votre e-mail :</p>
+  <input type="text" name="email">
+  <p>Votre message</p>
+  <textarea name="text" rows="6" cols-"35"></textarea><br/><br/>
+  <input type"submit" name="submit" value="Poster">
+</form>
